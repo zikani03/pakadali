d19699d53b49466ecf4a56647de02bf9ffe0bcd3
Zikani Nyirenda Mwase <zikani@nndi-tech.com>
2021-07-21 02:24:28 +0200

    Support 24 hour clock and improve timestamp parsing

--- src/main/java/me/zikani/labs/pakadali/api/whatsapp/ChatReader.java
+++ src/main/java/me/zikani/labs/pakadali/api/whatsapp/ChatReader.java
@@ -1,22 +1,31 @@
 package me.zikani.labs.pakadali.api.whatsapp;
 
+import org.slf4j.LoggerFactory;
+
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ChatReader {
     // Pattern to match the date times in the messages
-    static final Pattern messageLinePattern = Pattern.compile("^(?<timestamp>\\d{1,2}\\/\\d{1,2}\\/\\d{2,4},\\s\\d{1,2}:\\d{2}\\s(AM|PM|am|pm)\\s?\\-)\\s(?<sender>(\\w*[^:]\\s*?)+\\b:)\\s(?<message>.*$)");
-    static final Pattern datePattern = Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{2,4},\\s\\d{1,2}:\\d{2}\\s(AM|PM|am|pm)");
+    static final Pattern messageLinePattern = Pattern.compile("^(?<timestamp>\\d{1,2}\\/\\d{1,2}\\/\\d{2,4},\\s\\d{1,2}:\\d{2}\\s(AM|PM|am|pm)?\\s?\\-)\\s(?<sender>(\\w*[^:]\\s*?)+\\b:)\\s(?<message>.*$)");
+    static final Pattern datePattern = Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{2,4},\\s\\d{1,2}:\\d{2}\\s(AM|PM|am|pm)?");
     static final Pattern mediaOmittedPattern = Pattern.compile("\\<Media omitted\\>|Waiting for this message");
     static final Pattern atPhoneNumber = Pattern.compile("(@\\d{8,13})");
     static final Pattern justNumbers = Pattern.compile("\\d*\\s*\\d");
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/YY, HH:mm a -");
+    private static final List<SimpleDateFormat> DEFAULT_DATE_FORMAT_LIST = List.of(
+        new SimpleDateFormat("MM/dd/YY, HH:mm a -"),
+        new SimpleDateFormat("MM/dd/YY, HH:mm -")
+    );
+    // The data format that was successful, is for the first message we successfully parse
+    private SimpleDateFormat currentFormat = null;
+
 
     public List<Message> readMessages(InputStream inputStream) throws Exception {
         List<Message> messages = new ArrayList<>();
@@ -52,7 +61,7 @@ public class ChatReader {
         }
     }
 
-    public static Optional<Message> parseMessage(String messageLine) {
+    public Optional<Message> parseMessage(String messageLine) {
         var matcher = messageLinePattern.matcher(messageLine);
         if (matcher.matches()) {
             // TODO: String sender = sha256(matcher.group("sender").replace(" ", ""));
@@ -61,20 +70,37 @@ public class ChatReader {
                 sender = sender.replace(" ", "");
             }
             sender = sender.replace(":", "");
-
-            try {
-                long timestamp = dateFormat.parse(matcher.group("timestamp")).toInstant().toEpochMilli();
-                return Optional.of(new Message(
-                    sender,
-                    new String[0], // TODO: Parse mentions...
-                    timestamp,
-                    matcher.group("message")
-                ));
-            } catch (Exception e) {
-                throw new IllegalArgumentException(e);
+            long timestamp = -1;
+            if (currentFormat == null) {
+                for (SimpleDateFormat sdf : DEFAULT_DATE_FORMAT_LIST) {
+                    timestamp = parseTimestamp(matcher, sdf);
+                    if (timestamp != -1) {
+                        currentFormat = sdf;
+                        break;
+                    }
+                }
+            } else {
+                timestamp = parseTimestamp(matcher, currentFormat);
             }
+
+            return Optional.of(new Message(
+                sender,
+                new String[0], // TODO: Parse mentions...
+                timestamp,
+                matcher.group("message")
+            ));
+
         }
 
         return Optional.empty();
+    }
+
+    private long parseTimestamp(Matcher matcher, SimpleDateFormat sdf) {
+        try {
+            return sdf.parse(matcher.group("timestamp")).toInstant().toEpochMilli();
+        } catch(Exception e) {
+            LoggerFactory.getLogger(ChatReader.class).warn("Failed to parse timestamp: {} using {}", matcher.group("timestamp"), sdf.toPattern());
+        }
+        return -1;
     }
 }
